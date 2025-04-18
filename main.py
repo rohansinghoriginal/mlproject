6112e015d7913ff27beba9db61b36330207a284a
Rohan <rohansingh13902@gmail.com>
2025-04-19 02:30:03 +0530

    exception handling

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from sensor.exception import SensorException
-import os
 import sys
+import os
+from sensor.logger import logging
 
+#test function for exception
 def test_exception():
     try:
+        logging.info("Here is an error!!!")
         a=1/0
     except Exception as e:
         raise SensorException(e, sys)
 
-
+# testing exception 
 if __name__ == "__main__":
     try:
-        test_exception
+        test_exception()
     except Exception as e:
         print(e)
